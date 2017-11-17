a7c9e295ed32a1bca3610164ad31098641fff992
Kyle <kyle@kxnrl.com>
2017-11-17 15:49:47 +0800

    add error output in search.php

--- web/search.php
+++ web/search.php
@@ -12,6 +12,14 @@ $data = $api->search($_GET['s']);
 $result = json_decode($data);
 $_array = json_decode(json_encode($result, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES),true); //再把json字符串格式化为数组  
 
+if(!is_array($_array)){
+    $file=fopen(__DIR__ . '/ERROR.log', 'a+');
+    fputs($file, $_array);
+    fclose($file);
+    print_r($_array);
+    die(1);
+}
+
 // processing data
 $data = vdf_encode($_array['result'], true);
 
